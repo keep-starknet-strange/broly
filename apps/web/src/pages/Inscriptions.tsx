0411b5c3b9e0b2df0cf3160f1f7f62ff816dcb22
Brandon R <54774639+b-j-roberts@users.noreply.github.com>
2024-12-18 20:43:19 -0600

    dev: frontend warnings (#28)
    * Initialize wallet integration with starknetkit
    
    * Info screen, responsive, and add starknet-react
    
    * Cleanup icons
    
    * Fix icons shadows
    
    * Fix build warnings
    
    * Unused import

--- apps/web/src/pages/Inscriptions.tsx
+++ apps/web/src/pages/Inscriptions.tsx
@@ -5,7 +5,7 @@ import InscriptionRequestView from "../components/inscription/RequestView";
 import { getHotInscriptions, getNewInscriptions, getInscriptionRequests } from "../api/inscriptions";
 import { Pagination } from "../components/Pagination";
 
-function Inscritpions(props: any) {
+function Inscritpions(_props: any) {
   const filters = ["Hot", "New", "Rare"];
   const [activeFilter, setActiveFilter] = useState(filters[0]);
 
