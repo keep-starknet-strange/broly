00cf5716a47a41b78b092aee38dfd854ce12923f
Brandon Roberts <brandonjroberts22@gmail.com>
2025-01-22 18:06:15 -0600

    Update deploy instructions w/ contract address

--- apps/web/src/components/inscription/ImageLargeView.tsx
+++ apps/web/src/components/inscription/ImageLargeView.tsx
@@ -24,7 +24,7 @@ function InscriptionImageLargeView(props: any) {
     <div className="flex flex-col items-center justify-center h-full">
       <h3 className="text-lg View__header">Image</h3>
       <div className="flex flex-row items-center justify-center h-full w-full">
-        <img src={image} alt="inscription" className="object-contain h-[30rem] pixelated" />
+        <img src={image} alt="inscription" className="object-contain h-[30rem] pixelated View__pixelated" />
         {false && (
           <div className="flex flex-col items-center justify-around h-full px-4">
             <p>Normal</p>
