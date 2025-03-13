5297005e9a14925065a57364d7dedd42c3319316
Lana Ivina <svetlana.konstantinovna@gmail.com>
2025-03-13 17:30:49 +0400

    add buffer dependency

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: path.resolve(__dirname, 'node_modules/buffer/')
+    }
+  },
+  define: {
+    'global.Buffer': 'buffer.Buffer'
+  }
 })
